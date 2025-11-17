244dcd9343f860a1c6f228d389dc0bbe72c98c6f
AhdAtwya15 <ahddody31@gmail.com>
2025-11-18 00:02:44 +0200

    fix: remove setImageUrl completely - use Cloudinary direct URLs (final fix)

--- controllers/handlersFactory.js
+++ controllers/handlersFactory.js
@@ -1,34 +1,21 @@
+// handlersFactory.js
+
 const asyncHandler = require('express-async-handler');
 const ApiError = require('../utils/apiError');
 const ApiFeatures = require('../utils/apiFeatures');
-
-const setImageUrl = (doc) => {
-  if (doc.imageCover) {
-    const imageCoverUrl = `${process.env.BASE_URL}/products/${doc.imageCover}`;
-    doc.imageCover = imageCoverUrl;
-  }
-  if (doc.images) {
-    const images = [];
-    doc.images.forEach((image) => {
-      const imageUrl = `${process.env.BASE_URL}/products/${image}`;
-      images.push(imageUrl);
-    });
-    doc.images = images;
-  }
-};
 
 exports.deleteOne = (Model) =>
   asyncHandler(async (req, res, next) => {
     const document = await Model.findByIdAndDelete(req.params.id);
 
     if (!document) {
-      next(
+      return next(
         new ApiError(`No document found for this id: ${req.params.id}`, 404)
       );
     }
-    // To trigger 'remove' event when delete document
+
+    // Trigger 'remove' event (for middleware like deleting images from Cloudinary if needed)
     document.remove();
-    // 204 no content
     res.status(204).send();
   });
 
@@ -44,42 +31,33 @@ exports.updateOne = (Model) =>
       );
     }
 
-    // To trigger 'save' event when update document
-    const doc = await document.save();
+    // Trigger 'save' event if needed (e.g., for updatedAt)
+    await document.save();
 
-    if (doc.constructor.modelName === 'Product') {
-      setImageUrl(doc);
-    }
-    res.status(200).json({ data: doc });
+    res.status(200).json({ data: document });
   });
 
 exports.createOne = (Model) =>
   asyncHandler(async (req, res) => {
     const newDoc = await Model.create(req.body);
-
-    if (newDoc.constructor.modelName === 'Product') {
-      setImageUrl(newDoc);
-    }
     res.status(201).json({ data: newDoc });
   });
 
 exports.getOne = (Model, populateOpts) =>
   asyncHandler(async (req, res, next) => {
     const { id } = req.params;
-    // Build query
-    let query = Model.findById(id);
-    if (populateOpts) query = query.populate(populateOpts);
 
-    // Execute query
+    let query = Model.findById(id);
+    if (populateOpts) {
+      query = query.populate(populateOpts);
+    }
+
     const document = await query;
 
     if (!document) {
       return next(new ApiError(`No document for this id ${id}`, 404));
     }
 
-    if (document.constructor.modelName === 'Product') {
-      setImageUrl(document);
-    }
     res.status(200).json({ data: document });
   });
 
@@ -90,35 +68,27 @@ exports.getAll = (Model, modelName = '') =>
       filter = req.filterObject;
     }
 
-    // Build query
-    // const documentsCounts = await Model.countDocuments();
     const apiFeatures = new ApiFeatures(Model.find(filter), req.query)
       .filter()
       .search(modelName)
       .limitFields()
       .sort();
-    // .paginate();
 
-    // Apply pagination after filer and search
     const docsCount = await Model.countDocuments(apiFeatures.mongooseQuery);
     apiFeatures.paginate(docsCount);
 
-    // Execute query
     const { mongooseQuery, paginationResult } = apiFeatures;
     const documents = await mongooseQuery;
 
-    // Set Images url
-    if (Model.collection.collectionName === 'products') {
-      documents.forEach((doc) => setImageUrl(doc));
-    }
-    res
-      .status(200)
-      .json({ results: docsCount, paginationResult, data: documents });
+    res.status(200).json({
+      results: docsCount,
+      paginationResult,
+      data: documents,
+    });
   });
 
 exports.deleteAll = (Model) =>
-  asyncHandler(async (req, res, next) => {
+  asyncHandler(async (req, res) => {
     await Model.deleteMany();
-    // 204 no content
     res.status(204).send();
   });
